d12fb16ef3faca3ba600c375ce2da4ebd290b2ad
Gurumoorthy Raghupathy <gurumoorthy.raghupathy@wipro.com>
2020-02-05 14:28:45 +0000

    Initial checkin

---
+++ src/main/java/org/agileguru/baby/BabyApplication.java
@@ -0,0 +1,12 @@
+package org.agileguru.baby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BabyApplication.class, args);
+    }
+
+}
